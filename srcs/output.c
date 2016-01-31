b08f3afa1a430d510e8f8fdda092275fd91868e5
jcoignet <julian.coignet@gmail.com>
2016-01-31 21:02:34 +0100

    added conclusion to output

--- srcs/output.c
+++ srcs/output.c
@@ -1,14 +1,14 @@
 #include "ft_nmap.h"
 
-static void	print_scan_and_state(t_scan scan, t_pstate state)
+static void	print_scan_and_state(t_scan scan, t_pstate state, int isccl)
 {
     const char    *state_names[] = {
 	"UNTESTED",
 	"BEING_TESTED",
 	"OPEN",
 	"CLOSED",
-	"FILTERED",
 	"UNFILTERED",
+	"FILTERED",
 	"OPEN|FILTERED"
     };
     const char    *scan_names[] = {
@@ -19,21 +19,35 @@ static void	print_scan_and_state(t_scan scan, t_pstate state)
 	"ACK",
 	"UDP"
     };
-    printf("%s(%s) ", scan_names[scan], state_names[state]);
+    if (isccl == 0)
+        printf("%s(%s) ", scan_names[scan], state_names[state]);
+    else
+	printf("Conclusion %s ", state_names[state]);
 }
 
 static void	display_port_data(t_port *port, t_options *opts)
 {
     struct servent  *service;
     int	i = 0;
+    unsigned int ccl = STATE_OPENFILTERED;
 
     printf("Port %d => ", port->id);
     while (i < NB_SCAN)
     {
 	if (opts->scans[i] == 1)
-	    print_scan_and_state(i, port->states[i]);
+	{
+	    print_scan_and_state(i, port->states[i], 0);
+	    if (port->states[i] < ccl)
+		ccl = port->states[i];
+	}
 	i++;
     }
+    /* print conclusion
+     * open > closed > unfiltered > filtered > open|filtered
+     */
+   if (ccl < STATE_OPEN)
+	ccl = STATE_FILTERED;
+    print_scan_and_state(0, ccl, 1);
     service = getservbyport(htons(port->id), NULL);
     if (service != NULL)
 	printf("Service: %s.\n", service->s_name);
